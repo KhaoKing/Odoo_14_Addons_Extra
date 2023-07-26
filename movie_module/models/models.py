e9d313bef444108dbfbc1dbf2e6273c64004054e
Leo <leokhao2611@gmail.com>
2023-07-26 17:07:11 -0400

    [ADD] - Vistas Kanban, Form y Tree

--- movie_module/models/models.py
+++ movie_module/models/models.py
@@ -1,5 +1,4 @@
 # -*- coding: utf-8 -*-
-
 from odoo import models, fields
 
 
@@ -7,15 +6,23 @@ class movie_module_class(models.Model):
     _name = 'movie.movie_module'
     _description = 'List of movies most saw'
 
-    name_movie = fields.Char(string='Movie Name', required=True)
-    year_movie = fields.Integer(string='Movie Year')
-    rating_of_the_movie = fields.Float (string='Raiting of the Movie', compute="_value_pc", store=True)
-    duration = fields.Float(string='Duration')
-    genere = fields.Text(string='Genere')
+    name_movie = fields.Char(string='Name', required=True)
+    year_movie = fields.Char(string='Year of Release')
+    rating_movie = fields.Integer(string='Rating of the Movie (1-5)', help="""1 = Bad
+    2 = Not Bad
+    3 = Meh
+    4 = Good
+    5 = Excellent""")
+    duration = fields.Integer(string='Duration', help="Say how long the movie is")
+    image = fields.Binary(string="Imagen", help="This field is for upload an image in format PNG")
 
-
-
-
+    movie_selec = [
+        ('Opc1','Action'),
+        ('Opc2','Adventures'),
+        ('Opc3','Thriller'),
+        ('Opc4','Animation')
+    ]
+    genre = fields.Selection(movie_selec, string='Film Genre', required=True)
 
 
 
