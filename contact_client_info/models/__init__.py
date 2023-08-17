48f34652af330ef3937f042868afbaa5b9c34753
Leonardo Khaoim <LeoKhao2611@gmail.com>
2023-08-17 00:43:25 -0400

    [WIP] - Creacion del Modulo contact_client_info, mas inherit en su model

---
+++ contact_client_info/models/__init__.py
@@ -0,0 +1,3 @@
+# -*- coding: utf-8 -*-
+
+from . import add_info_client
---
+++ contact_client_info/models/add_info_client.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+from odoo import models, fields, api
+
+class SalesOrder (models.Model):
+    _inherit='sale.order'
