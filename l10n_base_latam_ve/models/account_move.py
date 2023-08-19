81c19d4be68b67238bd5f4ef8979dfb611f08e2b
Leonardo Khaoim <LeoKhao2611@gmail.com>
2023-08-19 00:10:56 -0400

    [WIP] - Se acomodo la logica de consulta de cliente, más se agrego 	el archivo que permite la tradcción (i18n)

--- l10n_base_latam_ve/models/account_move.py
+++ l10n_base_latam_ve/models/account_move.py
@@ -5,24 +5,28 @@ from odoo.exceptions import UserError
 class AccountMove(models.Model):
     _inherit = "account.move"
 
-    street = fields.Char(readonly=True)
-    street2 = fields.Char(readonly=True)
-    phone_number = fields.Char(readonly=True)
-    country_id = fields.Char(readonly=True)
-    vat = fields.Char(readonly=True)
+    street = fields.Char(readonly=True, string='Street')
+    street2 = fields.Char(readonly=True, string='Street 2')
+    phone_number = fields.Char(readonly=True, string='Phone Number')
+    country_id = fields.Char(readonly=True, string='Country ID')
+    vat = fields.Char(readonly=True, string='VAT')
 
     @api.onchange('partner_id')
     def _onchange_contact_info(self):
-        if self.partner_id.street and self.partner_id.vat == '':
-            raise UserError ('Este usuario no dispone de un RIF ni de alguna direccion, revise el contacto.')
-        else:
-            if self.partner_id:
-                self.write({
-                    'country_id': self.partner_id.country_id.id if self.partner_id.country_id else False,
-                    'street': self.partner_id.street,
-                    'street2': self.partner_id.street2,
-                    'phone_number': self.partner_id.phone,
-                    'vat': self.partner_id._get_vat(),
-                })
+        partner_id = self.partner_id
+        if partner_id:
+            street = self.partner_id.street
+            vat = self.partner_id._get_vat() 
+            print (street)
+            print (vat)   
+            if not street or not vat:
+                raise UserError ("Ops! This client doesn't have an Address or VAT defined. Check the information of Contact")
+            self.write({
+                'country_id': self.partner_id.country_id.id if self.partner_id.country_id else False,
+                'street': street,
+                'street2': self.partner_id.street2,
+                'phone_number': self.partner_id.phone,
+                'vat': vat,
+            })
 
 
